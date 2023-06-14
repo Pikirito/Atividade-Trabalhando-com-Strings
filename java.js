9b2c058a5315e3e03e033094c460b2b501228fe4
Eduardo <eduaraujp760@gmail.com>
2023-06-14 11:53:21 -0400

    Atividade: Trabalhando com Strings

---
+++ java.js
@@ -0,0 +1,30 @@
+//Exercicio 1
+let ola = 'Olá'
+let nome1 = "João"
+let nome2 = "Silva!"
+let saudacoes = ola +" "+ nome1 + " " + nome2
+console.log(saudacoes)
+
+//Exercicio 2
+let hello = "hello"
+let world = "world"
+let saida = hello +""+ world
+console.log(saida)
+
+//Exercicio 3
+let string1 = "12.5"
+let resultado1 = parseInt(string1)
+console.log(resultado1)
+
+//Exercicio 4
+let number1 = "12"
+let resultado = parseInt(number1)
+console.log(resultado)
+
+//Exercicio 5 
+let num1 = "12"
+let num2 = "3"
+let nuum1 = parseInt(num1)
+let nuum2 = parseInt(num2)
+let result = nuum1 + nuum2
+console.log(result)
